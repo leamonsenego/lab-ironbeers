effbbd3d26dd53e6d6c18ce756eabc2521b28052
leamonsenego <lea.monsenego@gmail.com>
2023-08-15 17:59:00 +0200

    done

--- app.js
+++ app.js
@@ -13,13 +13,62 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  let beersFromAPI = [];
+  const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+  const punkAPI = new PunkAPIWrapper();
+
+  punkAPI.getBeers()
+    .then((beer) => {
+      const beersFromAPI = beer.map((item) => {
+        return {
+          name: item.name,
+          image: item.image_url,
+          description: item.description,
+          tagline: item.tagline,
+        };
+      });
+     res.render('beers', {beers: beersFromAPI})
+    })
+    .catch((error) =>{
+      console.error(error)
+      res.status(500).send('Error fetching beers.')
+    });
+});
+
+
+
+app.get('/random-beer', (req, res) => {
+
+  const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+  const punkAPI = new PunkAPIWrapper()
+
+  punkAPI.getRandom()
+    .then((beer) => {
+      const randomIndex = Math.floor(Math.random() * beer.length)
+      const randomBeer = {
+        name: beer[randomIndex].name,
+        image_url: beer[randomIndex].image_url,
+        description: beer[randomIndex].description,
+        tagline: beer[randomIndex].tagline,
+        food_pairing: beer[randomIndex].food_pairing,
+        brewers_tips: beer[randomIndex].brewers_tips,
+      };
+      res.render('random-beer', {beer: randomBeer})
+    })
+  .catch((error) => {
+    console.error(error)
+    res.status(500).send('Error fetching beers.')
+  });
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
